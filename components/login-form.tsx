96f137fa4a8e3fe5cc6b697cbaec4abaa6a67cb1
Saksham Saraswat <sakshamsaraswat0208@gmail.com>
2026-03-31 21:27:31 -0400

    login page ui implemented

---
+++ components/login-form.tsx
@@ -0,0 +1,68 @@
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Log in to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to continue to Sitebench
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <span className="ml-auto text-sm text-muted-foreground">
+                    Forgot your password?
+                  </span>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit" className="w-full">
+                  Log In
+                </Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <Link href="/sign-up">Sign up</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
